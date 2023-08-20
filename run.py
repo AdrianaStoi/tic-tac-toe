200544624f44889f241b8c114c5868494f0502c6
AdrianaStoi <>
2023-08-20 17:10:13 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+game_instructions = """
+Welcome to Tic-Tac-Toe!
+Here’s how you can play the game :
+o	The game is played on a 3x3 game board. 
+o	The goal of the game is to form a line of three of your own marks
+    horizontally, vertically, or diagonally before the other player.
+o	You play the game against the computer.
+o	The computer plays using “X”s.
+o	You play the game using “0”s.
+Good luck!
+"""
+print(game_instructions)
