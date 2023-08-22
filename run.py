9fa26615724cc913c70f44ee1f7585676ee7fdc9
AdrianaStoi <>
2023-08-22 15:21:28 +0000

    Fix long lines and remove trailing whitespace

--- run.py
+++ run.py
@@ -87,18 +87,16 @@ def computer_move(board):
             for col in range(len(board[row])):
                 if board[row][col] == computer_choice:
                     computer_position_board = (row, col)
-                 
         if computer_position_board not in check_free_cell(board):
-            print(f"Cell {computer_choice} is already filled in, enter a different number.")
+            print(f"Cell {computer_choice} is already filled in,\
+enter a different number.")
             continue
 
         else:
             print("Computer's move:", computer_choice)
-   
         row, col = computer_position_board
         board[row][col] = "X"
         generate_board(board)
-      
         condition = False
 
     return check_win(board, computer_symbol, user_symbol)
@@ -115,8 +113,9 @@ def user_move(board):
 
     while condition:
         try:
-            user_input = int(input("Enter your move (only numbers between 1-9 accepted:"))
-        
+            user_input = int(input("Enter your move\
+(only numbers between 1-9 accepted:"))
+
         except ValueError:
             print("You must enter a number between 1 and 9!")
             continue
@@ -130,9 +129,10 @@ def user_move(board):
             for col in range(len(board[row])):
                 if board[row][col] == user_input:
                     user_position_board = (row, col)
-                   
+
         if user_position_board not in check_free_cell(board):
-            print(f"Cell {user_input} is already filled in, enter a different number.")
+            print(f"Cell {user_input} is already filled in,\
+enter a different number.")
             continue
 
         row, col = user_position_board
@@ -153,19 +153,19 @@ def check_win(board, computer_symbol, user_symbol):
 
     # Check the winner on rows and columns
     for i in range(3):
-        if (board[i][0] == board[i][1] == board[i][2]) and (board[i][0] in [computer_symbol, user_symbol]):
+        if (board[i][0] == board[i][1] == board[i][2]) and (board[i][0] in [computer_symbol, user_symbol]):  # noqa
             winner = board[i][0]
-        
-        if (board[0][i] == board[1][i] == board[2][i]) and (board[0][i] in [computer_symbol, user_symbol]):
+
+        if (board[0][i] == board[1][i] == board[2][i]) and (board[0][i] in [computer_symbol, user_symbol]):  # noqa
             winner = board[0][i]
 
     # Check the winner on diagonals
-    if (board[0][0] == board[1][1] == board[2][2]) and (board[0][0] in [computer_symbol, user_symbol]):
+    if (board[0][0] == board[1][1] == board[2][2]) and (board[0][0] in [computer_symbol, user_symbol]):  # noqa
         winner = board[0][0]
 
-    if (board[0][0] == board[1][1] == board[2][2]) and (board[0][2] in computer_symbol, user_symbol):
+    if (board[0][0] == board[1][1] == board[2][2]) and (board[0][2] in computer_symbol, user_symbol):  # noqa
         winner = board[0][2]
-  
+
     # Check for tie if there is no winner and if all cells are filled in.
     if not winner and not check_free_cell(board):
         winner = "Tie"
